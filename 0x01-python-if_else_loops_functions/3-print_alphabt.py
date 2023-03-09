171930ac8ae3cd1898d6e481d7c199b20e5832cc
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-03-09 13:34:08 -0800

    Update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,8 @@
 # 2-print_alphabet.py
 
 """Print the alphabet in lowercase, not followed by a new line."""
+
 for letter in range(97, 123):
-	if chr(letter) == "q" and chr(letter) == "e":
-		continue
-    print("{}".format(chr(letter)), end="")
+    if chr(letter) == "q" and chr(letter) == "e":
+        continue
+        print("{}".format(chr(letter)), end="")
