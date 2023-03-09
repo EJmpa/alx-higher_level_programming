5aa05f71b411a75a0d37bdbd2e9c1d2b732fa45c
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-03-09 13:36:32 -0800

    Update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -6,4 +6,4 @@
 for letter in range(97, 123):
     if chr(letter) == "q" and chr(letter) == "e":
         continue
-        print("{}".format(chr(letter)), end="")
+    print("{}".format(chr(letter)), end="")
