40dc17a9ea9083befeff39a7ccd910b545d73350
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-03-29 04:58:18 -0700

    Print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end = "")
-            count += 1
-        print()   
+            count += 1   
     except IndexError:
         print()
 
