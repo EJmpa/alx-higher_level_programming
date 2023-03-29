a20de67d64ea8856d80fa4bbc7c53afb0c64545f
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-03-29 11:11:27 -0700

    complete

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,12 @@
 def safe_print_division(a, b):
   try:
     result = a / b
+    return result
   except ZeroDivisionError:
       result =  None
+      return result
   finally:
       print("Inside result: {}".format(result))
-      return result
+
       
     
