ce20602fea6b365b1dcb0f9fe60a0deb6b8a08cf
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-03-29 08:17:06 -0700

    task_2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 def safe_print_list_integers(my_list=[], x=0):
-  count = 0
+    count = 0
     if(my_list):
         for i in range(x):
             try:
                 print("{:d}".format(my_list[i]), end='')
                 count += 1
-            except BaseException:
-                print()
-                return(count)
+            except (ValueError, TypeError):
+                pass
     print()
     return(count)
