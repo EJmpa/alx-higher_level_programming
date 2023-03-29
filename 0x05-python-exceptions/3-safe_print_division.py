1891dcd53d9d6c4f0f65455bb4e9ea6889822261
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-03-29 18:57:28 +0100

    task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
   try:
-    return a / b
+    result = a / b
    except ZeroDivisionError:
     return None
    finally:
-      print("{}".format(a / b))
+      print(Inside result: "{}".format(result))
+      return result
+      
     
