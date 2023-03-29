68f51dee88ff3d6d75cefa31b1191681a1133027
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-03-29 05:24:36 -0700

    Print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         print()
         return count
-    except IndexError:
-        print()
-        return count
+    except BaseException:
+        return i
 
