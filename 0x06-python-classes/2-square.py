34b89ba232655b7ebd60ccd8a29c0357d6b2a39f
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-03-31 16:30:22 +0100

    task_2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        try:
+          assert isinstance(size, int)
+        except:
+          raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
