e46ab7bc41854479a83297ed60a3db2ca12b25cd
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-03-31 16:14:10 +0100

    task_1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create a Square class"""
+class Square:
+    """Represents a square class"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+        self.__size = size
