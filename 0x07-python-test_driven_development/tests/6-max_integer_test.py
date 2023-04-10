fcacbe20813bcd4c718120f912f559422ca0c879
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-10 00:45:02 -0700

    Update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -46,7 +46,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_string(self):
         """Test a string."""
         string = "Chukwuemeka"
-        self.assertEqual(max_integer(string), 'u')
+        self.assertEqual(max_integer(string), 'w')
 
     def test_list_of_strings(self):
         """Test a list of strings."""
