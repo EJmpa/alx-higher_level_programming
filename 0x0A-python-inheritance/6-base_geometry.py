3af0d31f7ad213c0c1a287da9918bdf3aef2c8f7
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-11 10:43:59 -0700

    task_6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """This class represents a base geometry"""
+
+    def area(self):
+        """method yet to be implemented"""
+        raise Exception("area() is not implemented")
