7091442d3ca3a7b58b10a85be5f2186f8a3643b5
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-11 12:04:49 -0700

    task_11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry"""
+
+
+class BaseGeometry:
+    """this class represents a base geometry"""
+
+    def area(self):
+        """method not implemented yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates a value as an integer
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A class to define rectangle using BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Intialize class Rectangle parameters
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+        
+    def area(self):
+        """returns the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """returns the print() and str() representation of a Rectangle"""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
+    
+class Square(Rectangle):
+    """A subclass `"square"`"""
+    
+    def __init__(self, size):
+        """Initialize a new class `"square"`
+        """
+        super().__init__(size, size)
+        
+        self.integer_validator("size", size)
+        self.__size = size
