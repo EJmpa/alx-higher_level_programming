306b578c260a759186db11a5998c33b6d9613327
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-13 06:23:30 -0700

    task_1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file & return the number of characters
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
