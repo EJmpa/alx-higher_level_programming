32c74a9873b9366f36b0b0527f6e18496a33bda6
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-13 08:14:41 -0700

    task_11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,19 @@
+class Student:
+    """Defines a student by first name, last name, and age."""
+    def __init__(self, first_name, last_name, age):
+        """Instantiates a Student object with the given attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance with those in the given dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
