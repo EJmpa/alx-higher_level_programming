f9112f1e87dde301b1c7563b9fc05bfdeec8abb6
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-16 17:36:07 +0100

    class Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+#Importation of `Base` class from `'models/base.py'
+from models.base import Base
+
+class Rectangle(Base):
+    """class `'Rectangle'` that inherits
+    'Base' class features
+    """
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """`Rectangle` class initialzation"""
+        super().__init__()
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        
+    
+    #Assign easch private instance attributes with its own public getter and setter:
+    
+    @property
+    def width(self):
+        """Gets/Returns the value of width"""
+        return self.__width
+    
+    @property
+    def height(self):
+        """Gets/Returns the value of height"""
+        return self.__height
+    
+    @property
+    def x(self):
+        """Gets/Returns the value of x"""
+        return self.__x
+
+    
+    @width.setter
+    def width(self, value):
+        """Sets the value of width"""
+        self.__width = value
+
+
+    @height.setter
+    def height(self, value):
+        """Sets the value of height"""
+        self.__height = value
+        
+    @x.setter
+    def x(self, value):
+        """Sets the value of x"""
+        self.__x = value
+        
+    @y.setter
+    def x(self, value):
+        """Sets the value of y"""
+        self.__y = value
+        
+        
