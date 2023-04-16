2ce2bcf51ded0149ed63f6611c9a0190f34b2922
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-04-16 08:46:29 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This module stores a class named `'Base'`"""
+
+class Base:
+    """A Python class - 'Base' for evaluation"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """An dunder method(__init__()) for
+        initializing `'Base'` class arguments
+        """
+
+        if id != None:
+            self.id = id
+        self.id = __nb_objects + 1
+
+
