b4ea2db6afbe77d4b38345d7887ea920a9861810
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-19 16:59:28 +0100

    Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,10 @@ class Square(Rectangle):
     """A 'Square'` class that inherits the '`Rectangle'` class"""
     def __init__(self, size, x=0, y=0, id=None):
         """`'Square'` class initialization"""
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = id
         super().__init__(size, size, x, y, id)
         
     def __str__(self):
