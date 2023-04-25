7c2db1fa9252a53bf5ea843f3443518783162823
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-04-25 11:28:20 -0700

    task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,10 +3,11 @@
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """A function that writes an Object to a text file,
     using a JSON representation
     """
-    
-    with open(filename, "w", encoding = 'utf-8') as f:
+
+    with open(filename, "w", encoding='utf-8') as f:
         json.dump(my_obj, f)
