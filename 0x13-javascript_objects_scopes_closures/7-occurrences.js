cee6d598dcd401f7f4cb034fbca4e440606eaccb
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-15 14:30:00 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,10 +2,12 @@
 
 exports.nbOccurences = function(list, searchElement) {
   let count = 0;
+
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
   }
+
   return count;
 };
