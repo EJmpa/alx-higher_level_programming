ad24948de9c6a629011f2365d309a9d488bcf9a5
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-17 18:42:18 +0100

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+import sys
+import MySQLdb
+
+# Read MySQL credentials from command-line arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+# Connect to the MySQL server
+db = MySQLdb.connect(
+    host="localhost",
+    user=username,
+    passwd=password,
+    db=database,
+    port=3306
+)
+
+# Create a cursor object to execute SQL queries
+cursor = db.cursor()
+
+# Execute the SQL query to fetch all states from the states table
+query = "SELECT * FROM states ORDER BY id ASC"
+cursor.execute(query)
+
+# Fetch all the rows returned by the query
+rows = cursor.fetchall()
+
+# Print the states
+for row in rows:
+    print(row[1])
+
+# Close the cursor and database connection
+cursor.close()
+db.close()
