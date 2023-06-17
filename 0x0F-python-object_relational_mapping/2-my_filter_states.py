a6a6c15b519403253dbc816aeae8dc2e699747c7
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-17 21:02:34 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,49 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
+import sys
+import MySQLdb
 
+"""
+Access to the database and get the states
+from the database.
+"""
+
+if __name__ == '__main__':
+
+    # Read MySQL credentials from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the SQL query to fetch all states from the states table
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+
+    # Print the states
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
