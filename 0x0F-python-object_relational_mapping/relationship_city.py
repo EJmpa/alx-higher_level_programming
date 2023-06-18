86c3ad037aafdf49e1e77293f7c462606542e49c
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-18 12:10:01 +0100

    Create relationship_city.py

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script defines a City class
+to work with MySQLAlchemy ORM.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    """City class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
