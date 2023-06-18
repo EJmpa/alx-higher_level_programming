20270310afcb372b8fd68e42e04265eedcb1d056
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-18 10:14:35 +0100

    Create 11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+# Checks if the script is being executed as the main module.
+if __name__ == "__main__":
+    # Get the command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create the engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, database),
+        pool_pre_ping=True
+    )
+
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Create a new State object
+    new_state = State(name="Louisiana")
+
+    # Add the new State object to the session
+    session.add(new_state)
+
+    # Commit the session to persist the changes in the database
+    session.commit()
+
+    # Print the new state's id
+    print(new_state.id)
+
+    # Close the session
+    session.close()
