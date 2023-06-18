c92e32b73bc6181c11d57d79b18d576af4982ade
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-18 09:57:45 +0100

    Create 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+# Checks if the script is being executed as the main module.
+if __name__ == "__main__":
+    # Get the command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Create the engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, database),
+        pool_pre_ping=True
+    )
+
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Query the State object with the given name
+    state = session.query(State).filter(State.name == state_name).first()
+
+    # Print the result
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+
+    # Close the session
+    session.close()
