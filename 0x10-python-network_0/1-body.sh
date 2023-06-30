fc28d76cbef71d72182ff3434ef1279b6d50c4e2
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-06-30 17:10:45 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body of the response
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-if [[ $response -eq 200 ]]; then
-    curl -s "$1"
-fi
-
+curl -sL "$1"
