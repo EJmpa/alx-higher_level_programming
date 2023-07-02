01e16102b6a6c0761c1f9e2425f979c755f148c2
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-02 01:29:14 -0700

    task_4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches a URL and displays the body of the response using the requests package
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
