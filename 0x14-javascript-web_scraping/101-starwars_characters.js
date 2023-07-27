0b48f8eb90d5f4e94c3041ce77f2a5858c3ff76c
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-27 10:04:53 +0100

    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,45 +2,33 @@
 
 const request = require('request');
 
-async function fetchCharacterData(characterUrl) {
-  return new Promise((resolve, reject) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const characterData = JSON.parse(body);
-        resolve(characterData.name);
-      }
-    });
-  });
-}
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+let characters = [];
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./101-starwars_characters.js <Movie-ID>');
-  process.exit(1);
-}
-
-const movieID = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieID}/`;
-
-request(url, async (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      const movie = JSON.parse(body);
-      const characters = movie.characters;
-
-      for (const characterUrl of characters) {
-        try {
-          const characterName = await fetchCharacterData(characterUrl);
-          console.log(characterName);
-        } catch (err) {
-          console.error(err);
-        }
-      }
-    } else {
-      console.error(`Error: Status code ${response.statusCode}`);
-    }
+    console.log(error);
+    return;
   }
+
+  const data = JSON.parse(body);
+  characters = data.characters;
+  getCharacters(0);
 });
+
+const getCharacters = (index) => {
+  if (index === characters.length) {
+    return;
+  }
+
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    getCharacters(index + 1);
+  });
+};
