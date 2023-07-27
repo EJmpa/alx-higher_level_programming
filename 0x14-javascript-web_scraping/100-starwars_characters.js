af0966b4d9dda97db6742a27607aae4d0d9cf6f0
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-27 09:50:28 +0100

    Update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,46 +2,25 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./100-starwars_characters.js <Movie-ID>');
-  process.exit(1);
-}
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-const movieID = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-
-request(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      const movie = JSON.parse(body);
-      const characters = movie.characters;
+    console.log(error);
+    return;
+  }
 
-      function printCharacters(characters) {
-        if (characters.length === 0) {
-          return;
-        }
-
-        const characterUrl = characters.shift();
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error(error);
-          } else {
-            if (response.statusCode === 200) {
-              const character = JSON.parse(body);
-              console.log(character.name);
-              printCharacters(characters);
-            } else {
-              console.error(`Error: Status code ${response.statusCode}`);
-            }
-          }
-        });
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
       }
-
-      printCharacters(characters);
-    } else {
-      console.error(`Error: Status code ${response.statusCode}`);
-    }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
   }
 });
