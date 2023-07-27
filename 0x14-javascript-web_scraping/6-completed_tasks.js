741b14369170c6406b501c2f85bdc46e8cd9f5c9
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-27 09:37:49 +0100

    Update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,35 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
 
-request.get(url, { json: true }, (error, response, body) => {
+if (process.argv.length !== 3) {
+  console.error('Usage: ./6-completed_tasks.js <API-URL>');
+  process.exit(1);
+}
+
+const apiURL = process.argv[2];
+
+request(apiURL, (error, response, body) => {
   if (error) {
-    console.log(error);
-    return;
-  }
+    console.error(error);
+  } else {
+    if (response.statusCode === 200) {
+      const tasks = JSON.parse(body);
+      const completedTasksByUser = {};
 
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
-      }
+      tasks.forEach((task) => {
+        if (task.completed) {
+          if (completedTasksByUser[task.userId]) {
+            completedTasksByUser[task.userId]++;
+          } else {
+            completedTasksByUser[task.userId] = 1;
+          }
+        }
+      });
+
+      console.log(completedTasksByUser);
+    } else {
+      console.error(`Error: Status code ${response.statusCode}`);
     }
-  });
-  console.log(tasksCompleted);
+  }
 });
